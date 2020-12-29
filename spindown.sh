620fa28ca20c793e730635ddbc487933250559fb
AlexB <alex.bayer@gmail.com>
2020-12-29 21:15:53 +0100

    Update spindown.sh
    1. get available disk(s), not port(s) --> `synousbdisk -enum` instead `synodiskport -enum_port -usb`
    2. use exit code of smartctl instead of grep "ACTIVE"
    3. DSM notification added when hdd spins down
    4. unnecessary code removed

--- spindown.sh
+++ spindown.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
-declare -a USBPRT
-declare -a USBDRV
-declare -a WDIDLE
-TIMEOUT=600
-LOG=/var/log/wdspindown.log
 while true ; do
-    # check available USB ports
-    USBPRT=($(synodiskport -enum_port -usb $1))
-    for prt in ${USBPRT[*]}; do
-        # check disk mode (active, standby, not connected)
-        USBDRV=($(smartctl -n standby /dev/$prt | grep -e ACTIVE -e STANDBY))
-        if [[ ${USBDRV[3]} = "ACTIVE" ]] ; then
-            # if active mode, get idle time
-            WDIDLE=($(synodisk --get_idle /dev/$prt $1))
-            if [ ${WDIDLE[2]} -gt ${TIMEOUT} ] ; then
-                # if idle time greater as timeout, then spin down
-                echo "$(date +%Y%m%d-%H:%M:%S) : last access to /dev/$prt is ${WDIDLE[2]} second(s) ago. Spin down..." #>> $LOG
-                synodisk --usbstandby /dev/$prt
+    timeOut=600
+    usbDrvs=($(synousbdisk -enum | grep 'sd')) #  get available disk(s)
+    for drv in ${usbDrvs[*]}; do
+        if smartctl -q silent -n standby /dev/$drv ; then # get disk mode: active/idle - exit code 0, standby/notconnected - exit code 2
+            idleTime=($(synodisk --get_idle /dev/$drv $1)) # get idle time
+            if [ ${idleTime[2]} -gt ${timeOut} ] ; then # idle time greater as timeout? then spin down
+                synodisk --usbstandby /dev/$drv
+                echo "$(date +%Y%m%d-%H:%M:%S) : /dev/$drv is idle since ${idleTime[2]} second(s). Spin down..." >> /var/log/spindown.log
+                synodsmnotify @administrators "USB Disk" "Spin Down"
             else
-                # wait until idle time greater as timeout
-                ((WAIT=${TIMEOUT}-${WDIDLE[2]}))
-                echo "$(date +%Y%m%d-%H:%M:%S) : last access to /dev/$prt is ${WDIDLE[2]} second(s) ago. Waiting $WAIT second(s)..." #>> $LOG
-                sleep ${WAIT}
+                ((timeOut=${timeOut}-${idleTime[2]})) # wait just until idle time greater as timeout
             fi
-        elif [[ ${USBDRV[3]} = "STANDBY" ]] ; then
-            # if standby mode, then wait
-            echo "$(date +%Y%m%d-%H:%M:%S) : /dev/$prt is ${USBDRV[3]}" #>> $LOG
-            sleep ${TIMEOUT}
-        else
-            # it no disk connected, then skip
-            #echo "$(date +%Y%m%d-%H:%M:%S) : /dev/$prt is ${USBDRV[3]:-not connected}"
-            sleep 0
         fi
     done
+    sleep ${timeOut}
 done
