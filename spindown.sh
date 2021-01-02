dd4a43a8e037e16454dc154fbe2c2a6ca482f36b
AlexB <alex.bayer@gmail.com>
2021-01-02 20:12:14 +0100

    Update spindown.sh
    synodisk command fix (remove $1 due to conflict with start up command, /usr/local/etc/rc.d/spindown.sh start)

--- spindown.sh
+++ spindown.sh
@@ -4,7 +4,7 @@ while true ; do
     usbDrvs=($(synousbdisk -enum | grep 'sd')) #  get available disk(s)
     for drv in ${usbDrvs[*]}; do
         if smartctl -q silent -n standby /dev/$drv ; then # get disk mode: active/idle - exit code 0, standby/notconnected - exit code 2
-            idleTime=($(synodisk --get_idle /dev/$drv $1)) # get idle time
+            idleTime=($(synodisk --get_idle /dev/$drv)) # get idle time
             if [ ${idleTime[2]} -gt ${timeOut} ] ; then # idle time greater as timeout? then spin down
                 synodisk --usbstandby /dev/$drv
                 echo "$(date +%Y%m%d-%H:%M:%S) : /dev/$drv is idle since ${idleTime[2]} second(s). Spin down..." >> /var/log/spindown.log
