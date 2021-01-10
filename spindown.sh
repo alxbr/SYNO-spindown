2185a6bc3334e6967a68d57a86326d91f5bdd711
AlexB <alex.bayer@gmail.com>
2021-01-10 14:24:42 +0100

    Update spindown.sh
    Start/stop/status control options added, because according to Synology guide line https://help.synology.com/developer-guide/integrate_dsm/run_with_system_boot.html it must contain the options of "start" and "stop".

--- spindown.sh
+++ spindown.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+# 
+# /usr/local/etc/rc.d/spindown.sh
+
+pid_file=/var/run/spindown.pid
+log_file=/var/log/spindown.log
+
+start_spindown_ctl () {
+if [ -r $pid_file ]; then
+	read mypid < $pid_file
+	echo "$0 is already running. PID is $mypid"
+	exit 0
+else
+	echo $$ > $pid_file
+	echo "$(date +%Y%m%d-%H:%M:%S) : $0 is started. New PID is $$" >> $log_file
+fi
+
+trap 'stop_spindown_ctl' 2 # if started in interactive mode and ended with ctrl+c, the pid file should be deleted
+
 while true ; do
     timeOut=600
     usbDrvs=($(synousbdisk -enum | grep 'sd')) #  get available disk(s)
@@ -7,8 +25,7 @@ while true ; do
             idleTime=($(synodisk --get_idle /dev/$drv)) # get idle time
             if [ ${idleTime[2]} -gt ${timeOut} ] ; then # idle time greater as timeout? then spin down
                 synodisk --usbstandby /dev/$drv
-                echo "$(date +%Y%m%d-%H:%M:%S) : /dev/$drv is idle since ${idleTime[2]} second(s). Spin down..." >> /var/log/spindown.log
-                synodsmnotify @administrators "USB Disk" "Spin Down"
+                echo "$(date +%Y%m%d-%H:%M:%S) : /dev/$drv is idle since 10 minutes now. Spin down..." >> $log_file
             else
                 ((timeOut=${timeOut}-${idleTime[2]})) # wait just until idle time greater as timeout
             fi
@@ -16,3 +33,36 @@ while true ; do
     done
     sleep ${timeOut}
 done
+}
+
+stop_spindown_ctl () {
+if [ -w $pid_file ] ; then
+    read mypid < $pid_file
+	rm $pid_file
+	echo "$(date +%Y%m%d-%H:%M:%S) : $0 (PID $mypid) is stopped now" >> $log_file
+	kill $mypid
+else
+	echo "$0 is not running"
+fi
+}
+
+status () {
+usbDrvs=($(synousbdisk -enum | grep 'sd'))
+for drv in ${usbDrvs[*]}; do
+	synodisk --get_idle /dev/$drv
+	smartctl -n standby /dev/$drv | grep -e mode -e Mode
+done
+if [ -r $pid_file ] ; then
+	read mypid < $pid_file
+	echo "$0 is still running. PID is $mypid"
+else	
+	echo "$0 is not running"
+fi
+}
+
+case $1 in
+start) start_spindown_ctl;;
+stop) stop_spindown_ctl;;
+status) status;;
+*) echo "Usage: $0 start|stop|status";;
+esac
